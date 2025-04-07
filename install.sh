2ffde9cfdcaeffc5d6205dc8425dafbc0c9d6f0e
Vicrorege <anteytim@gmail.com>
2025-04-07 19:46:02 +0300

    installer

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+echo "Instalation started..."
+if [ "$EUID" -ne 0 ]; then
+    exec sudo "$0" "$@"
+    exit $?
+fi
+echo "Generating venv.."
+python3 -m venv venv
+echo "Venv generated succsessful!"
+echo "Required libs installing..."
+./venv/bin/pip3 install pyTelegramBotAPI
+echo "Libs installed succsessful!"
+
+nano ./tgcp.service
+cp ./tgcp.service /etc/systemd/system/tgcp.service
+
+nano ./config.json
+systemctl enable tgcp
+systemctl start tgcp
+echo "Complited!"
